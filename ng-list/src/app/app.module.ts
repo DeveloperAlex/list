73499008898f2c7b3cc77ca293f24a787da43cc6
developeralex@gmail.com <developeralex@gmail.com>
2017-03-20 18:51:46 +0000

    cleaning

--- ng-list/src/app/app.module.ts
+++ ng-list/src/app/app.module.ts
@@ -10,9 +10,9 @@ import { AngularFireModule } from 'angularfire2';
 import { firebaseConfig } from '../environments/firebase.config';  // https://www.udemy.com/angular-firebase-application/learn/v4/t/lecture/5798940?start=0  //Lecture 21 @5:17 of 7:24.
 import { MaterialModule, MdProgressSpinnerModule } from '@angular/material';
 
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/switchMap';
+// import 'rxjs/add/operator/do';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/switchMap';
 
 import { AlertModule } from 'ng2-bootstrap';  // https://github.com/valor-software/ng2-bootstrap/blob/development/docs/getting-started/ng-cli.md
 import { nvD3 } from 'ng2-nvd3';
@@ -30,40 +30,8 @@ import { WishService } from './services/wish.service';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { DockerComponent } from './components/docker/docker.component';
-//import { SpinnerComponent } from './core/spinner/spinner.component';
 import { SpinnerComponent } from './core';
 import { D3Component } from './components/d3/d3.component';
-
-// export const firebaseConfig = {
-//   apiKey: "AIzaSyBDQ0nM2cJFSYmHbYPx4j90-RacHnYA5PU",
-//   authDomain: "ngfirebase-ebc9d.firebaseapp.com",
-//   databaseURL: "https://ngfirebase-ebc9d.firebaseio.com",
-//   storageBucket: "ngfirebase-ebc9d.appspot.com",
-//   messagingSenderId: "1006700743662"
-// };
-
-// We'll need to add: "<router-outlet></router-outlet>" to the html - to use routing.
-//  See bottom of cheatsheet for ideas:  https://angular.io/docs/ts/latest/guide/cheatsheet.html
-
-// const routes: Routes = [
-//   { path: 'netflixlist', component: NetflixListComponent },
-//   { path: 'wishlist',    component: WishListComponent },
-//   { path: 'chat', loadChildren: 'app/+group-chat/group-chat.module#GroupChatModule'}
-// ];
-
-// { path: 'home',        component: AppComponent },
-// { path: '', redirectTo: '/home', pathMatch: 'full' }
-// { path: '**', component: PageNotFoundComponent }
-
-// const routes: Routes = [
-//     { path: 'netflixlist', component: NetflixListComponent },
-//     { path: 'wishlist', component: WishListComponent }
-// ];
-
-
-//  <base href="/">  //Caused doubling of ui (both of these do).
-//  { path: '', component: AppComponent },
-//  { path: '**', component: AppComponent }  //Caused doubling of ui (both of these do).
 
 
 // https://plnkr.co/edit/LCsiXOtzSedGZDbGQ3f8?p=preview
@@ -73,6 +41,7 @@ export class MyHammerConfig extends HammerGestureConfig  {
   }
 }
 
+//  See bottom of cheatsheet for ideas:  https://angular.io/docs/ts/latest/guide/cheatsheet.html
 
 // Notes:
 //  declarations = get all of the app's classes decorated w/ @Component. Directives & Pipes must be added to declarations too.
@@ -91,7 +60,6 @@ export class MyHammerConfig extends HammerGestureConfig  {
     SpinnerComponent,
     D3Component,
     nvD3
-    //SpinnerComponent
   ],
   imports: [
     BrowserModule,
