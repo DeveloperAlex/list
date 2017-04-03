23af8f68e4e3d80cdfda9920577ee1d49441338a
developeralex@gmail.com <developeralex@gmail.com>
2017-04-03 00:20:42 +0000

    more

--- ng-list/src/main.ts
+++ ng-list/src/main.ts
@@ -14,7 +14,7 @@ if (environment.production) {
 // agGrid.initialiseAgGridWithAngular1(angular);
 //
 // https://github.com/ceolter/ag-grid-angular-example/search?utf8=%E2%9C%93&q=license&type=
-//
+// Angular 4:
 import { LicenseManager } from "ag-grid-enterprise/main";
 LicenseManager.setLicenseKey("SHI_on_behalf_of_AIG_Site_10Devs23_January_2018__MTUxNjY2NTYwMDAwMA==8b08adcfc0c4594bacd6eb06e88e1d3b");
 
