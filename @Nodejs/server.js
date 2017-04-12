c3d3f8721537fc42661247894bcdf95967c2e960
developeralex@gmail.com <developeralex@gmail.com>
2017-04-12 13:04:01 +0000

    node running

--- @Nodejs/server.js
+++ @Nodejs/server.js
@@ -6,9 +6,30 @@
 
 var express = require("express");
 var app = express();
+var cors = require("cors");  //https://github.com/expressjs/cors
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 app.use(bodyParser.json());
+
+
+app.use(cors());
+// OPTIONS verb needs to be handled too (for CORS - preflight requests).
+// app.use(function(req, res, next) {  //Perhaps this would be better - than adding a cors npm module (above).
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+//   if (req.method === 'OPTIONS') {
+//     return res.send(200);
+//   } else {
+//     return next();
+//   }
+// });
+
+
+// Below works for: https://ng2-list-001-nodejs-developeralex.c9users.io/
+app.get('/', function (req, res, next) {
+  res.json({msg: 'Yippy - node.js express restful service is running.'})
+})
 
 
 var apiRoutes = require('./routes/apiRoutes');
@@ -16,7 +37,8 @@ app.use('/api', apiRoutes);
 
 
 // var server = app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-var server = app.listen(8082, process.env.IP || "0.0.0.0", function(){
+//var server = app.listen(8082, process.env.IP || "0.0.0.0", function(){
+var server = app.listen(8080, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Node.js Express RESTful server listening at", addr.address + ":" + addr.port);
 });
