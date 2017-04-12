31896595b83a5f3d84751f54737b84543d7f337c
developeralex@gmail.com <developeralex@gmail.com>
2017-04-12 02:23:55 +0000

    http service starting

--- @Angular/src/app/services/nodejs.service.ts
+++ @Angular/src/app/services/nodejs.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Http, HTTP_PROVIDERS } from '@angular/http';
+import 'rxjs/add/operator/map'
+
 
 @Injectable()
 export class NodejsService {
 
-  constructor() { }
+  constructor(http: Http) { 
+    
+  }
 
 }
+
+
+
+
+/*
+// https://angular.io/docs/ts/latest/guide/server-communication.html
+
+// https://angular.io/docs/ts/latest/guide/server-communication.html#!#the-herolistcomponent-class
+// https://angular.io/docs/ts/latest/guide/server-communication.html#!#fetch-data-with-_http-get-_
+
+// https://angular.io/docs/ts/latest/api/http/index/Http-class.html
+
+@Component({
+  selector: 'http-app',
+  viewProviders: [HTTP_PROVIDERS],
+  templateUrl: 'people.html'
+})
+class PeopleComponent {
+  constructor(http: Http) {
+    http.get('people.json')
+      // Call map on the response observable to get the parsed people object
+      .map(res => res.json())
+      // Subscribe to the observable to get the parsed people object and attach it to the
+      // component
+      .subscribe(people => this.people = people);
+  }
+}
+*/
