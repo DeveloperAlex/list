51e11bafc70a1295c0aa177835ca14545e6eb9fe
developeralex@gmail.com <developeralex@gmail.com>
2017-04-18 13:59:36 +0000

    Save to Mongo works

--- @Nodejs/routes/widgetRoute.js
+++ @Nodejs/routes/widgetRoute.js
@@ -7,11 +7,22 @@ var router = express.Router();
 var daMongo = require('./_daMongo');
 var daMongoConnection = daMongo();
 
+var Widget = require('../models/widgetModel');
 
 
+// https://mlab.com/databases/nglist/collections/widgets
 // TODO: Change this from a "get" to a "post" http verb -- for now keep the "get" to make it easier to develop/test with.
 // https://ng2-list-001-nodejs-developeralex.c9users.io/api/widget/post/hairdryer  ==>  "You created 'blah' in mongo database"
 router.get('/post/:name', function (req, res) {
+  
+  var widget = new Widget({ name: req.params.name });
+  widget.save(function(err) {
+    if (err) {
+      console.error(err);
+    } else {
+    console.log(`Success! Saved '${req.params.name}' to mongo database`);
+    }
+  });
   
   res.status(201).send(`You created '${req.params.name}' in mongo database`);
 });
