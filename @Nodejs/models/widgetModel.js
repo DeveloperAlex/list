0c1843d0bf71410a0343f65c10bd582a619c6704
developeralex@gmail.com <developeralex@gmail.com>
2017-04-18 16:27:49 +0000

    improved mongo model

--- @Nodejs/models/widgetModel.js
+++ @Nodejs/models/widgetModel.js
@@ -3,29 +3,62 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 //---------------------------------------------------------
+// validators:
+var descriptionValidator = [
+  function(val) {
+    return (val.trim().length >= 3);
+  },
+  'Description must be at least 3 characters long'
+];
+
+//---------------------------------------------------------
+// schema:
 var widgetSchema = new Schema({
-  name: {type: String, index: true},
-  description: {type: String},
-  color: {type: String},
-  price: {type: Number, index: true},
-  date: { type: Date, default: Date.now },
+  name:        { type: String, index: true, trim: true, required: true },
+  description: { type: String, trim: true, validate: descriptionValidator },
+  color:       { type: String, trim: true },  // required: true, default: 'white', enum: ['red', 'white', 'blue']
+  price:       { type: Number, min: 0 },
+  quantity:    { type: Number },
+  tags: {
+    tag: { type: String, trim: true }
+  }
+}, {timestamps: true});
+
+//---------------------------------------------------------
+// middleware:
+widgetSchema.pre('validate', function(next) {  //http://mongoosejs.com/docs/api.html#model_Model-save
+  console.log('Validating...');  //Added in case I want to debug/inspect "this".
+  next();
 });
 
-// middleware
 widgetSchema.pre('save', function (next) {
   console.log(`About to save: ${this.get('name')}`);
   next();
 });
 
+//---------------------------------------------------------
+// methods:
 widgetSchema.methods.toString = function() {
   return `{ name: "${this.name}" }`;
 };
 
-
 //---------------------------------------------------------
-
+// model:
 var widgetModel = mongoose.model('Widget', widgetSchema);  // Mongoose will create the model for your widgets collection, not your widget collection.
 
+widgetModel.on('index', function (error) {
+  if (error) {  //If error is truthy, index build failed
+    console.log('widgetModel index error', error);
+  } else {
+    console.log('widgetModel index success');
+  }
+});
+
+widgetModel.schema.options.emitIndexErrors; // true  //http://mongoosejs.com/docs/guide.html#emitIndexErrors
+widgetModel.on('error', function(error) {
+  // gets an error whenever index build fails
+  console.log('widgetModel index detail error', error);
+});
 
 //---------------------------------------------------------
 
