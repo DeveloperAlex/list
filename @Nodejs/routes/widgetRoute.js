59e72a820032e30bf13f2e73a8a011e5ed52b1a1
developeralex@gmail.com <developeralex@gmail.com>
2017-04-20 15:24:37 +0000

    delete by name works

--- @Nodejs/routes/widgetRoute.js
+++ @Nodejs/routes/widgetRoute.js
@@ -10,6 +10,10 @@ var daMongoConnection = daMongo();
 var Widget = require('../models/widgetModel');
 
 
+
+// ------------------------------------------------------------------------------------------------
+
+
 // TODO: Change this from a "get" to a "post" http verb -- for now keep the "get" to make it easier to develop/test with. Obviously don't name it post.
 // https://mlab.com/databases/nglist/collections/widgets
 // https://ng2-list-001-nodejs-developeralex.c9users.io/api/widget/post/hairdryer  ==>  "You created 'blah' in mongo database"
@@ -26,6 +30,35 @@ router.get('/post/:name', function (req, res) {
   });
 });
 
+// ------------------------------------------------------------------------------------------------
+
+router.get('/delete/:name', function (req, res) {
+  Widget.find({name:req.params.name}).exec(function(err, widgets) {
+    if (err) {
+      console.error(`Find returns an error= ${err}`);
+      res.status(500).send(err);
+    } 
+
+    if (widgets.length === 1) {
+      var widget = widgets[0];
+      console.log('Deleting', widget);
+      
+      Widget.findByIdAndRemove(widget._id, function(err) {
+        if (err) {
+          res.status(500).json({info: 'error during delete', error: err});
+        };
+        res.status(200).json({info: 'Success deleting'});
+      });
+      
+    } else {
+      res.status(404).json({info: 'Delete encountered a problem'});
+    }
+
+  });
+      
+});
+
+// ------------------------------------------------------------------------------------------------
 
 function find(req, res, name) {
   Widget.find(name).sort({name: 'asc'}).limit(100).exec(function(err, widgets) {
@@ -49,6 +82,9 @@ function find(req, res, name) {
 router.get('/find/:name', function (req, res) {
   var regex = new RegExp(req.params.name, "i");
   find(req, res, {name: regex});
+
+  // Another way to do case-insensitive searches in Mongo (only in newer versions):  
+  // find(req, res, { $text: { $search: req.params.name, $caseSensitive: false, $diacriticSensitive:false } });  // https://docs.mongodb.com/manual/reference/operator/query/text/#text
 });
 
 // https://mlab.com/databases/nglist/collections/widgets
@@ -57,5 +93,6 @@ router.get('/find', function (req, res) {
   find(req, res, {});
 });
 
+// ------------------------------------------------------------------------------------------------
 
 module.exports = router;
