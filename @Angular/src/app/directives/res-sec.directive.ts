cf53178fc2ba851fa1b6f1fc63556c1a4d4725ca
developeralex@gmail.com <developeralex@gmail.com>
2017-04-24 18:46:57 +0000

    attribute directive

--- @Angular/src/app/directives/res-sec.directive.ts
+++ @Angular/src/app/directives/res-sec.directive.ts
@@ -1,20 +1,33 @@
-import { Directive, HostBinding, HostListener, Input } from '@angular/core';
-
-@Directive({
-  selector: '[ressec]'
-})
-export class ResSecDirective {
-  constructor() { }
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit } from '@angular/core';
 
 // Currently just implemented on "Add Wish" button on WishList Component.
 // This is used to modify the behavior of Save buttons and such. It's an Attribute Directive.
 
-  @Input() ressec
-  
+@Directive({  // Attribute Directive
+  selector: '[ressec]'
+  // host: {
+  //   '(click)': 'onClick($event)'
+  // }
+})
+export class ResSecDirective implements OnInit {
+  @Input() ressec = () => {};  // `ressec="{type:'save'}"`  // Works
+
+  constructor(private el: ElementRef) {}
+
+  ngOnInit() {
+    this.el.nativeElement.style.color = 'darkblue';
+    this.el.nativeElement.style.backgroundColor = 'lightgoldenrodyellow';  // 'lightblue';
+  }
+
+
   @HostListener('mouseover') onMouseEnter() {
     console.log(`Directive ressec fired onMouseEnter`)
   }
   
   // @HostBinding() innerText = "Text changed by ressec directive";  // Works
+
+  // @HostBinding() get innerText() {  // Works - takes `ressec="{type:'save'}"` and sets add button to "{type:'save'}".
+  //   return this.ressec;
+  // }
 
 }
